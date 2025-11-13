062021962d8c3345f9129a37a70aff7d87354269
pathgenixedu <pathgenixedu@gmail.com>
2025-11-13 10:36:33 +0000

    no result then what to do

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCxsVzMe-0CUS1-2tO803SV3cDx5cTIaiQ",
+  authDomain: "studio-2237527215-3623e.firebaseapp.com",
+  projectId: "studio-2237527215-3623e",
+  storageBucket: "studio-2237527215-3623e.appspot.com",
+  messagingSenderId: "1019072312283",
+  appId: "1:1019072312283:web:38a3d787acca7fece26dfe"
 };
 
 // Initialize Firebase
